73fa93c8819d2f5d447cf9a539e183daef372c37
Anaelise Huaniri Romero <aanesita@gmail.com>
2026-04-10 10:52:17 -0400

    Se agrego piramide simple de numeros

---
+++ Ejercicios de Clase/1er Parcial/PiramideNumericaSimple/PiramideSimple.java
@@ -0,0 +1,22 @@
+
+/*
+ * 1
+ * 12
+ * 123
+   */
+import java.util.Scanner;
+public class PiramideSimple
+{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca nro de filas");
+        int filas=sc.nextInt();
+        for(int i=1;i<=filas;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
