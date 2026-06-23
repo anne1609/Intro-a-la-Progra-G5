fd534f2feab5bb92f97693cd90631717124bdb1c
Anaelise Huaniri Romero <aanesita@gmail.com>
2026-06-23 09:00:37 -0400

    Se actualizo ejercicio Fifa

--- Examenes Pasados/Segundo parcial/9) Fifa/Fifa.java
+++ Examenes Pasados/Segundo parcial/9) Fifa/Fifa.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
     Fifa(){
         paises=new Pais[TAM_MAX];
         indice=0;
-        /*paises=new Pais[TAM_MAX];
+        paises=new Pais[TAM_MAX];
         indice=0;
         Scanner sc= new Scanner(System.in);
         System.out.println("Introduzca la cantidad de paises iniciales: ");
@@ -44,7 +44,7 @@ import java.util.Scanner;
            
             paises[indice]=new Pais(nombre,cantFutbolistas,colorCamiseta,anioFundacion,nombrePresidente);
             indice++;
-        }*/
+        }
         
         } 
     public  boolean existePais(String nombre){
@@ -71,5 +71,92 @@ import java.util.Scanner;
         System.out.println("No hay paises para mostrar");
     }
 }
+
+    public void visualizarPaises() {
+        if (indice >0) {
+            for (int i = 0; i < indice; i++) {
+                paises[i].mostrarPais();
+            }
+        }else{
+         System.out.println("No hay países registrados.");
+        }
+    }
+
+    public void añadirPais() {
+        Scanner sc = new Scanner(System.in);
+
+     if (indice < TAM_MAX) {
+        String nombre;
+        boolean existe;
+
+        do {
+            System.out.print("Nombre del país: ");
+            nombre = sc.nextLine();
+
+            existe = existePais(nombre);
+
+            if (existe) {
+                System.out.println("Ese país ya está registrado.");
+            }
+
+        } while (existe);
+
+        System.out.print("Cantidad de futbolistas: ");
+        int cantFutbolistas = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Color de camiseta: ");
+        String colorCamiseta = sc.nextLine();
+
+        System.out.print("Año de fundación: ");
+        int anioFundacion = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Nombre del presidente: ");
+        String nombrePresi = sc.nextLine();
+
+        paises[indice] = new Pais(
+                nombre,
+                cantFutbolistas,
+                colorCamiseta,
+                anioFundacion,
+                nombrePresi
+        );
+
+        indice++;
+
+        System.out.println("País añadido");
+      }else{
+       System.out.println("No se pueden añadir más países");  
+      }
+    }
+
+    public int buscarPais(String nombre) {
+        int pos=-1;
+        for (int i = 0; i < indice; i++) {
+            if (paises[i].nombre.equalsIgnoreCase(nombre)) {
+                pos=i;
+                break;
+            }
+        }
+        return pos;
+    }
+    
+    public void eliminarPais(String nombre) {
+       int pos = buscarPais(nombre);
+        if (pos == -1) {
+            System.out.println("País no encontrado.");
+        }else{
+
+        for (int i = pos; i < indice - 1; i++) {
+            paises[i] = paises[i + 1];
+        }
+
+        paises[indice - 1] = null;
+        indice--;
+
+        System.out.println("País eliminado correctamente.");
+       }
+    }
 }
 
